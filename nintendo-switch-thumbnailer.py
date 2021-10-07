d614652b75215611401fc2529927b081ffa62b55
Rhythm Lunatic <kirbyrulez@att.net>
2021-10-06 20:11:51 -0500

    cache thumbs this time

--- nintendo-switch-thumbnailer.py
+++ nintendo-switch-thumbnailer.py
@@ -31,7 +31,10 @@ def downloadNewDatabase():
 			t.write(r.content)
 			print("got new database, saved to "+TITLES)
 
-
+def writeImage(imgData):
+	img = cv2.resize(imgData,(HEIGHT,HEIGHT))
+	cv2.imwrite(sys.argv[3],img)
+	print("Wrote image to "+sys.argv[3])
 
 HEIGHT = int(sys.argv[2])
 #print(sys.argv[1])
@@ -41,25 +44,47 @@ if not match:
 	sys.exit(-1)
 titleID = match.group(1)
 
-if not os.path.isfile(TITLES) or time.time()-os.stat(TITLES).st_mtime > 604800:
-	downloadNewDatabase()
+iconsDir= os.path.join(os.path.expanduser('~'),'.cache','switchIcons')
+if not os.path.isdir(iconsDir):
+	os.mkdir(iconsDir)
 
-if USE_COMPRESSION:
-	with gzip.open(TITLES,"r") as db:
-		j = json.load(db)
+thumbPath = os.path.join(iconsDir,titleID+".jpg")
+if os.path.isfile(thumbPath):
+	imgData = cv2.imread(thumbPath)
+	writeImage(imgData)
 else:
-	with open(TITLES,'r') as db:
-		j = json.load(db)
+	if not os.path.isfile(TITLES) or time.time()-os.stat(TITLES).st_mtime > 604800:
+		downloadNewDatabase()
+
+	if USE_COMPRESSION:
+		with gzip.open(TITLES,"r") as db:
+			j = json.load(db)
+	else:
+		with open(TITLES,'r') as db:
+			j = json.load(db)
+
+	#print(j[titleID])
+	if titleID not in j:
+		print(titleID +" not present in database")
+		sys.exit(-1)
+	elif 'iconUrl' not in j[titleID] or j[titleID]['iconUrl'] is None:
+		print(titleID+" has no icon.")
+		sys.exit(-1)
+	
+	r = requests.get(j[titleID]['iconUrl'])
+	if r.ok:
+		with open(thumbPath,'wb') as t:
+			t.write(r.content)
+		npB = numpy.frombuffer(r.content,dtype=numpy.uint8)
+		imgData = cv2.imdecode(npB,flags=1)
+		writeImage(imgData)
+		
+	else:
+		print("Failed to obtain image!")
+		sys.exit(-1)
 
 
-if titleID not in j:
-	print(titleID +" not present in database")
-	sys.exit(-1)
-r = requests.get(j[titleID]['iconUrl'])
-if r.ok:
-	npB = numpy.frombuffer(r.content,dtype=numpy.uint8)
-	img = cv2.resize(cv2.imdecode(npB,flags=1),(HEIGHT,HEIGHT))
-	cv2.imwrite(sys.argv[3],img)
-	print("Wrote image to "+sys.argv[3])
+
+
 	#print(db[titleID])
 #requests.get('https://tinfoil.media/repo/db/',
